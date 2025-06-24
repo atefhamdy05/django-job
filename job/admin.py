c33c0bddb781ab693f4d16d115bfe3c75a30624c
Atef <atefhamdy05@gmail.com>
2025-06-25 02:23:25 +0300

    add urls

--- job/admin.py
+++ job/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import job
+from .models import job ,Category
+
 admin.site.register(job)
+admin.site.register(Category)
+
 # Register your models here.
